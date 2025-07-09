5111af399f1d339b71189265f3c29f80f61c1250
Mohammed Ismail <mohammedismail72425@gmail.com>
2025-07-10 01:12:11 +0530

    Add agents table, agents API, and agents UI with loading and error states

---
+++ src/app/(dashboard)/agents/page.tsx
@@ -0,0 +1,30 @@
+import React, { Suspense } from "react";
+
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { getQueryClient, trpc } from "@/trpc/server";
+
+import { ErrorBoundary } from "react-error-boundary";
+
+import {
+  AgentsView,
+  AgentsViewError,
+  AgentsViewLoading,
+} from "@/modules/agents/ui/views/agents-view";
+
+const AgentsPage = async () => {
+  const queryClient = getQueryClient();
+
+  void queryClient.prefetchQuery(trpc.agents.getMany.queryOptions());
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <ErrorBoundary fallback={<AgentsViewError />}>
+        <Suspense fallback={<AgentsViewLoading />}>
+          <AgentsView />
+        </Suspense>
+      </ErrorBoundary>
+    </HydrationBoundary>
+  );
+};
+
+export default AgentsPage;
--- src/modules/home/ui/views/home-view.tsx
+++ src/modules/home/ui/views/home-view.tsx
@@ -1,18 +1,5 @@
 "use client";
 
-import { useTRPC } from "@/trpc/client";
-import { useQuery } from "@tanstack/react-query";
-
 export const HomeView = () => {
-  const trpc = useTRPC();
-
-  const { data, isLoading } = useQuery(
-    trpc.hello.queryOptions({ text: "Mohammed Ismail" })
-  );
-
-  if (isLoading || !data) {
-    return <div>Loading...</div>;
-  }
-
-  return <div className="flex flex-col p-4 gap-y-4">{data.greeting}</div>;
+  return <div className="flex flex-col p-4 gap-y-4">Home View</div>;
 };
