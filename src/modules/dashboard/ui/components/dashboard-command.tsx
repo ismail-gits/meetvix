0d21f9fd41eb586cc5284a358603247c55f50bab
Mohammed Ismail <mohammedismail72425@gmail.com>
2025-07-10 18:33:58 +0530

    Add CommandResponsiveDialog and ResponseDialog components with mobile support

--- src/modules/dashboard/ui/components/dashboard-command.tsx
+++ src/modules/dashboard/ui/components/dashboard-command.tsx
@@ -1,10 +1,10 @@
 import { Dispatch, SetStateAction } from "react";
 
 import {
-  CommandDialog,
   CommandInput,
   CommandItem,
   CommandList,
+  CommandResponsiveDialog,
 } from "@/components/ui/command";
 
 interface DashboardCommandProps {
@@ -14,11 +14,11 @@ interface DashboardCommandProps {
 
 export const DashboardCommand = ({ open, setOpen }: DashboardCommandProps) => {
   return (
-    <CommandDialog open={open} onOpenChange={setOpen}>
+    <CommandResponsiveDialog open={open} onOpenChange={setOpen}>
       <CommandInput placeholder="Find a meeting or agent" />
       <CommandList>
         <CommandItem>Test</CommandItem>
       </CommandList>
-    </CommandDialog>
+    </CommandResponsiveDialog>
   );
 };
